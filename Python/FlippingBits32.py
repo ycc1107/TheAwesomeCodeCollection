2cae99ba22a1920e4ec0b3302991dd2392e61edc
Cheng <cy699@nyu.edu>
2015-02-26 12:00:22 -0500

    add des

--- Python/FlippingBits32.py
+++ Python/FlippingBits32.py
@@ -1,3 +1,10 @@
+'''
+You will be given a list of 32 bits unsigned integers. 
+You are required to output the list of the unsigned integers you get by 
+flipping bits in its binary representation 
+(i.e. unset bits must be set, and set bits must be unset).
+'''
+
 def flippingBits(a):
     # format bit. if no longer then 32 bits then fill with 0 
     bitA = [i for i in '{0:032b}'.format(a)]
