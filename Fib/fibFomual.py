a9495da185017f44a8fba4110cfe01adc5b32422
Cheng <cy699@nyu.edu>
2015-06-04 00:57:14 -0400

    Update fibFomual.py

--- Fib/fibFomual.py
+++ Fib/fibFomual.py
@@ -2,3 +2,12 @@ def fib(n):
   res = ((1+math.sqrt(5))/2)**n- ((1-math.sqrt(5))/2)**n
   res /= math.sqrt(5) 
   return res
+
+
+def fib(n):
+  temp,res =0,1
+  while res <n:
+    yield res
+    res,temp = res+temp,res
+
+[i for i in fib(n)]
